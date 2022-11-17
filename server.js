277f8e0841bc9f1e94c5722a22b37e5d6eaa98f9
James Vrtis <jamesvrtis@caskradmins-mbp.mynetworksettings.com>
2022-11-17 09:59:13 -0500

    asdasd

--- server.js
+++ server.js
@@ -3,20 +3,25 @@
 // =======================================
 const express = require('express');
 const app = express();
-const port = 3000;
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser')
-const methodOverride = require('method-override');
+let PORT = 3000;
+if(process.env.PORT){
+	PORT = process.env.PORT
+}
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'))
-app.use(methodOverride('_method'));
+// const port = 3000;
+// const mongoose = require('mongoose');
+// const bodyParser = require('body-parser')
+// const methodOverride = require('method-override');
+
+// app.use(express.urlencoded({extended: true}));
+// app.use(express.static('public'))
+// app.use(methodOverride('_method'));
 
 // =======================================
 // Must remain on bottom
 // =======================================
-const appRouter = require("./controllers/routes.js");
-app.use("/",appRouter);
+// const appRouter = require("./controllers/routes.js");
+// app.use("/",appRouter);
 
 // mongoose.connection.dropDatabase();
 
@@ -24,10 +29,15 @@ app.use("/",appRouter);
 //  console log commands for listening 
 //  and connectivity
 // =======================================
-app.listen(port, () => {
-    console.log(`Movies app listening on port: ${port}`)
-  });
 
-mongoose.connect('mongodb://localhost:27017/Movies', () => {
-  console.log('The connection with mongod is established')
-});
+// mongoose.connect('mongodb://localhost:27017/Movies', () => {
+//   console.log('The connection with mongod is established')
+// });
+
+app.get('/', (req, res)=>{
+	res.send('hi');
+})
+
+app.listen(PORT, ()=>{
+	console.log('listening...');
+})
